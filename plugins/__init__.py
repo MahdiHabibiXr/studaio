5217613a053a6720c3c405e421575077395edb97
MahdiHabibiXr <themahdihabibi@gmail.com>
2023-08-25 14:24:23 +0330

    redis

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,9 @@
 import redis
 
 query = redis.StrictRedis(
-    host='localhost',
+    host='redis.studaio.svc',
     port=6379,
+    password='lHsXtUaX2bK4J0G36uKVaLfoz9KyyeBJ'
     db=0,
     decode_responses=True
 )
