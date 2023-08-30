0fc0a996662d2865b511e4573d006fe0fecbaa44
MahdiHabibiXr <themahdihabibi@gmail.com>
2023-08-30 03:47:15 +0330

    yep

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,4 +11,4 @@ admin = 791927771
 name = 'StudAIo'
 api_url = 'http://127.0.0.1:7923'
 botname = 'studaiobot'
-bannerfid = 'AgACAgQAAxkBAAIGnWTud0hxAgMJ4Nq_nLJutSD1icfzAAIrvTEbfSt4Uzk-Cquxi5OjAAgBAAMCAAN5AAceBA'
+bannerfid = 'AgACAgQAAxkBAAIMH2TuikJDrDMWanIpiEIVFxxkRoolAALSwDEb15dwU2tYOgVk4mKZAAgBAAMCAAN5AAceBA'
